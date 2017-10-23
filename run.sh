6195ef5749992cbf3f8dcced1dd42016c36f2b4a
JD <jkd28@pitt.edu>
2017-10-23 10:10:18 -0400

    Modify script to account for compilation errors

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 echo "Compiling Tests..."
-javac -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar *.java
-echo "Executing Tests..."
-java -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar TestRunner
+if javac -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar *.java; then
+    echo "Executing Tests..."
+    java -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar TestRunner
+else
+    echo "TESTS FAILED TO COMPILE, NO TESTS EXECUTED"
+fi
